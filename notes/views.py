287e6c9f8d258a0d4eb82d1c221050d0a3a53c09
Joshua Baka <fatow.jay@gmail.com>
2025-07-05 22:57:29 +0000

    api: notes/views.py

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Teacher, LessonNote, Feedback
+from .serializers import TeacherSerializer, LessonNoteSerializer, FeedbackSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+  queryset = Teacher.objects.all()
+  serializer_class = TeacherSerializer
+
+class LessonNoteViewSet(viewsets.ModelViewSet):
+  queryset = LessonNote.objects.all()
+  serializer_class = LessonNoteSerializer
+
+class FeedbackViewSet(viewsets.ModelViewSet):
+  queryset = Feedback.objects.all()
+  serializer_class = FeedbackSerializer
