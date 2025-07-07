1cb07c09ed79d88a34645d46ff62678a1a7a4a09
Joshua Baka <fatow.jay@gmail.com>
2025-07-07 01:47:10 +0000

    rest framework configuration

--- ai_lesson_reviewer/settings.py
+++ ai_lesson_reviewer/settings.py
@@ -42,8 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
